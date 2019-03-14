aff43551ced2a6646accfde24445accffbd6ec72
ektigah <ektigah@gmail.com>
2019-03-14 01:16:36 -0400

    before refactoring Idea

--- app/assets/javascripts/components/ideas_container.js.jsx
+++ app/assets/javascripts/components/ideas_container.js.jsx
@@ -1,9 +1,48 @@
 class IdeasContainer extends React.Component {
-  render() {
-    return (
-      <div>
-        Ideas
-      </div>
-    )
+  
+
+constructor(props) {
+  super(props)
+  this.state = {
+    ideas: []
   }
+}  
+ 
+  
+    componentDidMount(){
+    fetch('/api/v1/ideas.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ ideas: data }) });
+  }
+ 
+  
+  
+  render(){
+
+ var ideas = this.state.ideas.map((idea) => {
+return (
+  <div className="tile" key={idea.id} >
+            <h4>{idea.title}</h4>
+            <p>{idea.body}</p>
+          </div>
+
+)
+
+ })
+ 
+  return(
+   <div>
+
+    
+   {ideas}
+  </div>
+ )
+ 
+ 
+
+}
+  
+  
+  
+  
 }
