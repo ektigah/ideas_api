a45958f7bb4a2c806ac359acf37c63ca010e8e0b
ektigah <ektigah@gmail.com>
2019-03-14 00:27:59 -0400

    first commit-added axios

---
+++ app/assets/javascripts/components/_main_js.jsx
@@ -0,0 +1,12 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Idea Board</h1>
+        </div>
+        <IdeasContainer />
+      </div>
+    );
+  }
+}
---
+++ app/assets/javascripts/components/ideas_container.js.jsx
@@ -0,0 +1,9 @@
+class IdeasContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        Ideas
+      </div>
+    )
+  }
+}
